f95d95bef2e8e88898f8f8e72f556331204e2e01
Ceyda <ceydaceyhan2001@gmail.com>
2023-10-24 13:41:14 +0300

    Recent ve all expenses ekranındaki harcamaların silinmesi veya güncellenmesi sağlandı.

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,6 +1,15 @@
+import { useContext } from "react";
 import ExpensesOuput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
 
 function AllExpenses() {
-  return <ExpensesOuput expensesPeriod="Total" />;
+  const expensesCtx = useContext(ExpensesContext);
+  return (
+    <ExpensesOuput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found!"
+    />
+  );
 }
 export default AllExpenses;
