cadb989241d7ea6068ab7c914c1d3855b151a686
Ceyda <ceydaceyhan2001@gmail.com>
2023-10-24 09:52:25 +0300

    Uygulamanın başlangıç ekranının ve edit ekranının tasarımı yapıldı.

---
+++ screens/AllExpenses.js
@@ -0,0 +1,6 @@
+import ExpensesOuput from "../components/ExpensesOutput/ExpensesOutput";
+
+function AllExpenses() {
+  return <ExpensesOuput expensesPeriod="Total" />;
+}
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,6 @@
+import ExpensesOuput from "../components/ExpensesOutput/ExpensesOutput";
+
+function RecentExpenses() {
+  return <ExpensesOuput expensesPeriod="Last 7 days." />;
+}
+export default RecentExpenses;
